5befc117235800c8977942b1e90ffffe8324fe76
giovanni <jovona.giovanni.1234@gmail.com>
2025-03-22 15:50:09 +0200

    modules

---
+++ vpc/main.tf
@@ -0,0 +1,33 @@
+resource "aws_key_pair" "my_key" {
+    key_name   = "my_key"
+    public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "web_server" {
+    ami           = var.image_id
+    instance_type = "t2.micro" 
+    subnet_id = aws_subnet.public.id
+    key_name = aws_key_pair.my_key.key_name
+    
+    user_data = <<-EOF
+                            #!/bin/bash
+                            sudo apt update -y
+                            sudo apt install apache2 -y
+                            EOF
+
+
+
+    vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "gio73bucket" 
+    instance_type = "t2.micro" 
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+
